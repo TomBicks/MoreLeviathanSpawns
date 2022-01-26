188082249bfd6f37114a14214e5369de4d14b92a
Brett <brett84c@gmail.com>
2022-01-26 13:59:30 -0500

    changed from attempting json back to xml

--- MoreReaperSpawns/MoreReaperSpawns.cs
+++ MoreReaperSpawns/MoreReaperSpawns.cs
@@ -1,7 +1,10 @@
 ﻿using HarmonyLib;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using SMLHelper.V2.Handlers;
 using System;
 using System.IO;
+using System.Xml.Serialization;
 using Logger = QModManager.Utility.Logger;
 
 namespace MoreReaperSpawns
@@ -16,85 +19,94 @@ namespace MoreReaperSpawns
             // Check to see if this is the player
             if (__instance.GetType() == typeof(Player))
             {
-                CheckFile();
+                string filepath = Path.Combine(SaveLoadManager.GetTemporarySavePath(), "spawnData.xml");
+                //string filepath = @"C:\Users\Brett\Documents\spawnData.json";
+                Logger.Log(Logger.Level.Info, "Save file location" + SaveLoadManager.GetTemporarySavePath());
+
+                if (!File.Exists(filepath))
+                {
+                    Logger.Log(Logger.Level.Info, "MoreReaperSpawns initialised for the first time. Creating spawns and saving json...");
+                    SeedCreatureCoordsAndSaveJSON(filepath);
+                } else
+                {
+                    Logger.Log(Logger.Level.Info, "Json found. Loading spawns from json file...");
+                }
+                SpawnCreatures(filepath);
             }
         }
 
-        static void CheckFile()
-        {
-            //var file = Directory.GetCurrentDirectory() + "/QMods/MoreReaperSpawns/spawns.txt";
-            var file = Path.Combine(SaveLoadManager.GetTemporarySavePath(), "MoreReaperSpawns");
-            if (File.Exists(file))
-            {
-                // already initialized, return to prevent from spawn duplications.
-                Logger.Log(Logger.Level.Info, "MoreReaperSpawns file was found");
-                Logger.Log(Logger.Level.Info, $"file: {file}");
-                return;
-            }
-
-            File.Create(file);
-            Logger.Log(Logger.Level.Info, "MoreReaperSpawns initialised for the first time");
-            spawnReapers();
-        }
-
-        static void spawnReapers()
+        static void SeedCreatureCoordsAndSaveJSON(string filepath)
         {
             //Random number generator will spawn reapers within a min/max area so they're never exactly the same
             //Useful resource for figuring out general coordinates: https://subnauticamap.io
 
+            SpawnCoords spawnCoords = new SpawnCoords();
+
             //Floating Islands Reaper
-            float[] floatingIslandsCoords = {
-                (float)new Random().Next(-1000, -500),
-                (float)new Random().Next(-100, -60),
-                (float)new Random().Next(-1300, -750)
+            spawnCoords.Coords[0] = new float[3]{
+                new Random().Next(-1000, -500),
+                new Random().Next(-100, -60),
+                new Random().Next(-1300, -750)
             };
-            Logger.Log(Logger.Level.Info, $"Floating Islands Reaper Coords: {floatingIslandsCoords[0]+ " " + floatingIslandsCoords[1] + " " + floatingIslandsCoords[2]}");
-            CoordinatedSpawnsHandler.RegisterCoordinatedSpawn(new SpawnInfo(TechType.ReaperLeviathan, new UnityEngine.Vector3(floatingIslandsCoords[0], floatingIslandsCoords[1], floatingIslandsCoords[2])));
 
             //Underwater Islands Reaper
-            float[] underwaterIslandsCoords = {
-                (float)new Random().Next(-385, 50),
-                (float)new Random().Next(-150, -100),
-                (float)new Random().Next(825, 1120)
+            spawnCoords.Coords[1] = new float[3]{
+                new Random().Next(-385, 50),
+                new Random().Next(-150, -100),
+                new Random().Next(825, 1120)
             };
-            Logger.Log(Logger.Level.Info, $"Underwater Islands Reaper Coords: {underwaterIslandsCoords[0] + " " + underwaterIslandsCoords[1] + " " + underwaterIslandsCoords[2]}");
-            CoordinatedSpawnsHandler.RegisterCoordinatedSpawn(new SpawnInfo(TechType.ReaperLeviathan, new UnityEngine.Vector3(underwaterIslandsCoords[0], underwaterIslandsCoords[1], underwaterIslandsCoords[2])));
 
             //Mushroom Forest Reaper
-            float[] mushroomForestCoords = {
-                (float)new Random().Next(-1250, -900),
-                (float)new Random().Next(-100, -40),
-                (float)new Random().Next(600, 900)
+            spawnCoords.Coords[2] = new float[3]{
+                new Random().Next(-1250, -900),
+                new Random().Next(-100, -40),
+                new Random().Next(600, 900)
             };
-            Logger.Log(Logger.Level.Info, $"Mushroom Forest Reaper Coords: {mushroomForestCoords[0] + " " + mushroomForestCoords[1] + " " + mushroomForestCoords[2]}");
-            CoordinatedSpawnsHandler.RegisterCoordinatedSpawn(new SpawnInfo(TechType.ReaperLeviathan, new UnityEngine.Vector3(mushroomForestCoords[0], mushroomForestCoords[1], mushroomForestCoords[2])));
 
             //Blood Kelp Reaper
-            float[] bloodKelpCoords = {
-                (float)new Random().Next(-1300, -1000),
-                (float)new Random().Next(-120, -70),
-                (float)new Random().Next(-600, -400)
+            spawnCoords.Coords[3] = new float[3]{
+                new Random().Next(-1300, -1000),
+                new Random().Next(-120, -70),
+                new Random().Next(-600, -400)
             };
-            Logger.Log(Logger.Level.Info, $"Blood Kelp Reaper Coords: {bloodKelpCoords[0] + " " + bloodKelpCoords[1] + " " + bloodKelpCoords[2]}");
-            CoordinatedSpawnsHandler.RegisterCoordinatedSpawn(new SpawnInfo(TechType.ReaperLeviathan, new UnityEngine.Vector3(bloodKelpCoords[0], bloodKelpCoords[1], bloodKelpCoords[2])));
 
             //Blood Kelp 2 Reaper
-            float[] bloodKelp2Coords = {
-                (float)new Random().Next(-900, -500),
-                (float)new Random().Next(-120, -70),
-                (float)new Random().Next(1180, 1600)
+            spawnCoords.Coords[4] = new float[3]{
+                new Random().Next(-900, -500),
+                new Random().Next(-120, -70),
+                new Random().Next(1180, 1600)
             };
-            Logger.Log(Logger.Level.Info, $"Blood Kelp 2 Reaper Coords: {bloodKelp2Coords[0] + " " + bloodKelp2Coords[1] + " " + bloodKelp2Coords[2]}");
-            CoordinatedSpawnsHandler.RegisterCoordinatedSpawn(new SpawnInfo(TechType.ReaperLeviathan, new UnityEngine.Vector3(bloodKelp2Coords[0], bloodKelp2Coords[1], bloodKelp2Coords[2])));
 
             //Grassy Plateau Reaper
-            float[] grassyPlateauCoords = {
-                (float)new Random().Next(400, 640),
-                (float)new Random().Next(-70, -40),
-                (float)new Random().Next(150, 280)
+            spawnCoords.Coords[5] = new float[3]{
+                new Random().Next(400, 640),
+                new Random().Next(-70, -40),
+                new Random().Next(150, 280)
             };
-            Logger.Log(Logger.Level.Info, $"Grassy Plateau Reaper Coords: {grassyPlateauCoords[0] + " " + grassyPlateauCoords[1] + " " + grassyPlateauCoords[2]}");
-            CoordinatedSpawnsHandler.RegisterCoordinatedSpawn(new SpawnInfo(TechType.ReaperLeviathan, new UnityEngine.Vector3(grassyPlateauCoords[0], grassyPlateauCoords[1], grassyPlateauCoords[2])));
+
+            XmlSerializer writer = new XmlSerializer(typeof(SpawnCoords));
+            FileStream file = File.Create(filepath);
+            writer.Serialize(file, spawnCoords);
+            file.Close();
+            Logger.Log(Logger.Level.Info, $"xml file path: {filepath}");
         }
+
+        static void SpawnCreatures(string filepath)
+        {
+            XmlSerializer reader = new XmlSerializer(typeof(SpawnCoords));
+            StreamReader file = new StreamReader(filepath);
+            SpawnCoords spawnCoords = (SpawnCoords) reader.Deserialize(file);
+            file.Close();
+            for (int i = 0; i < spawnCoords.Coords.Length; i++)
+            {
+                Logger.Log(Logger.Level.Info, $"Spawn #{i}: " + spawnCoords.Coords[i][0] + " " + spawnCoords.Coords[i][1] + " " + spawnCoords.Coords[i][2]);
+                CoordinatedSpawnsHandler.RegisterCoordinatedSpawn(new SpawnInfo(TechType.ReaperLeviathan, new UnityEngine.Vector3(spawnCoords.Coords[i][0], spawnCoords.Coords[i][1], spawnCoords.Coords[i][2])));
+            }
+        }
+    }
+    [Serializable]
+    public class SpawnCoords
+    {
+        public float[][] Coords = new float[6][];
     }
 }
