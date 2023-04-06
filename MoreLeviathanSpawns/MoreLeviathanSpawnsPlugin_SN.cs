f69cfe10f17e7254d4c51943d0c868be63cc665e
Thomas Bickley <49082698+TomBicks@users.noreply.github.com>
2023-04-06 18:22:02 +1000

    Changed default SpawnIntensity to max
    SpawnIntensity of both leviathans is now at max by default, for testing purposes.
    Will change this back to default of 3 after testing is complete.

--- MoreLeviathanSpawns/MoreLeviathanSpawnsPlugin_SN.cs
+++ MoreLeviathanSpawns/MoreLeviathanSpawnsPlugin_SN.cs
@@ -31,9 +31,9 @@ namespace MoreLeviathanSpawns
     [Menu("More Leviathan Spawns")]
     public class Config : SMLHelper.V2.Json.ConfigFile
     {
-        [Slider("Reaper Spawn Intensity", Min = 0F, Max = 6F, DefaultValue = 3F, Step = 1F, Id = "reaperSpawnIntensity", Tooltip = "Defines general intensity of additional reaper leviathan spawns to add to the game. A value of 1 will add roughly 2 - 4 spawns. A value of 6 will add roughly 20 - 23 spawns. A value of 0 will add no additional reaper leviathan spawns to game."), OnChange(nameof(SpawnIntensityChanged))]
+        [Slider("Reaper Spawn Intensity", Min = 0F, Max = 6F, DefaultValue = 6F, Step = 1F, Id = "reaperSpawnIntensity", Tooltip = "Defines general intensity of additional reaper leviathan spawns to add to the game. A value of 1 will add roughly 2 - 4 spawns. A value of 6 will add roughly 20 - 23 spawns. A value of 0 will add no additional reaper leviathan spawns to game."), OnChange(nameof(SpawnIntensityChanged))]
         public float ReaperSpawnIntensity = 3F;
-        [Slider("Ghost Spawn Intensity", Min = 0F, Max = 6F, DefaultValue = 3F, Step = 1F, Id = "ghostSpawnIntensity", Tooltip = "Defines general intensity of additional ghost leviathan spawns to add to the game. A value of 1 will add roughly 1 - 3 spawns. A value of 6 will add roughly 12 - 14 spawns. A value of 0 will add no additional ghost leviathan spawns to game."), OnChange(nameof(SpawnIntensityChanged))]
+        [Slider("Ghost Spawn Intensity", Min = 0F, Max = 6F, DefaultValue = 6F, Step = 1F, Id = "ghostSpawnIntensity", Tooltip = "Defines general intensity of additional ghost leviathan spawns to add to the game. A value of 1 will add roughly 1 - 3 spawns. A value of 6 will add roughly 12 - 14 spawns. A value of 0 will add no additional ghost leviathan spawns to game."), OnChange(nameof(SpawnIntensityChanged))]
         public float GhostSpawnIntensity = 3F;
         [Toggle("Always randomize spawns", Id = "alwaysRandomize", Tooltip = "By default, spawn locations are chosen randomly then saved and remain static for rest of playthrough. If this option is checked, spawns will always randomize when opening that save file."), OnChange(nameof(ToggleChanged))]
         public bool AlwaysRandomized = false;
