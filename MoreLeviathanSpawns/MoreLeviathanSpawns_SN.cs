f1b5647547961b8c2e8416ad68ff68bc2db6b8b2
Thomas Bickley <tombicks@outlook.com>
2024-03-01 15:49:31 +1100

    Coordinates now start as Vectors, random selection having issues
    Have made the selection of coordinates of both Reaper and Ghost Leviathans now start as Vector3, as opposed to float arrays with all 3 coordinates inside.
    Have made a class, GhostCoord, in order to accomodate this change whilst still keeping the 'Adult or Juvenile' value.
    Finally, have started work on instead randomly selecting from the coord array and adding that to the save file. However, the more I do this, the more I suspect the coord array should be a list. Next commit will likely be just that.

--- MoreLeviathanSpawns/MoreLeviathanSpawns_SN.cs
+++ MoreLeviathanSpawns/MoreLeviathanSpawns_SN.cs
@@ -72,12 +72,21 @@ namespace MoreLeviathanSpawns
             logger.Log(LogLevel.Info, $"Loading {reaperSpawnTotal} of {spawnData.ReaperCoords.Length} total reaper spawns");
             logger.Log(LogLevel.Info, $"Loading {ghostSpawnTotal} of {spawnData.GhostCoordsAndType.Length} total ghost spawns");
 
-            //load reaper spawns
-            for(int i = 0; i < reaperSpawnTotal; i++)
+            //Create an array as long as the total amount of spawns that will registered
+            saveCoords.ReaperCoords = new UnityEngine.Vector3[reaperSpawnTotal];
+            saveCoords.GhostCoords = new UnityEngine.Vector3[ghostSpawnTotal];
+
+            //Used to generate random selection of spawns to add to the new save file
+            System.Random rnd = new System.Random();
+
+            //Randomly select amount of reaper spawns equal to reaperSpawnTotal
+            for (int i = 0; i < reaperSpawnTotal; i++)
             {
-                logger.Log(LogLevel.Info, $"Reaper spawn #{i + 1} - Coords: {spawnData.ReaperCoords[i][0]} {spawnData.ReaperCoords[i][1]} {spawnData.ReaperCoords[i][2]}");
-                CoordinatedSpawnsHandler.RegisterCoordinatedSpawn(new SpawnInfo(TechType.ReaperLeviathan, new UnityEngine.Vector3(spawnData.ReaperCoords[i][0], spawnData.ReaperCoords[i][1], spawnData.ReaperCoords[i][2])));
-                
+                int j = rnd.Next(0, spawnData.ReaperCoords.Length - 1);
+                logger.Log(LogLevel.Info, $"Random selection of Reaper Coord #{j}");
+                logger.Log(LogLevel.Info, $"Reaper spawn #{i + 1} - Coords: {spawnData.ReaperCoords[j]}");
+                saveCoords.ReaperCoords[i] = spawnData.ReaperCoords[j];
+                //CoordinatedSpawnsHandler.RegisterCoordinatedSpawn(new SpawnInfo(TechType.ReaperLeviathan, new UnityEngine.Vector3(spawnData.ReaperCoords[i][0], spawnData.ReaperCoords[i][1], spawnData.ReaperCoords[i][2])));
             }
 
             //load ghost spawns
@@ -85,7 +94,7 @@ namespace MoreLeviathanSpawns
             {
                 TechType creatureType = new TechType();
                 string ghostType = "Adult";
-                switch (spawnData.GhostCoordsAndType[i][3])
+                switch (spawnData.GhostCoordsAndType[i].GhostType)
                 {
                     case 1://Ghost (Adult)
                         creatureType = TechType.GhostLeviathan;
@@ -95,8 +104,8 @@ namespace MoreLeviathanSpawns
                         ghostType = "Juvenile";
                         break;
                 }
-                logger.Log(LogLevel.Info, $"Ghost ({ghostType}) spawn #{i + 1} - Coords: {spawnData.GhostCoordsAndType[i][0]} {spawnData.GhostCoordsAndType[i][1]} {spawnData.GhostCoordsAndType[i][2]}");
-                CoordinatedSpawnsHandler.RegisterCoordinatedSpawn(new SpawnInfo(creatureType, new UnityEngine.Vector3(spawnData.GhostCoordsAndType[i][0], spawnData.GhostCoordsAndType[i][1], spawnData.GhostCoordsAndType[i][2])));
+                logger.Log(LogLevel.Info, $"Ghost ({ghostType}) spawn #{i + 1} - Coords: {spawnData.GhostCoordsAndType[i].Coords}");
+                //CoordinatedSpawnsHandler.RegisterCoordinatedSpawn(new SpawnInfo(creatureType, new UnityEngine.Vector3(spawnData.GhostCoordsAndType[i][0], spawnData.GhostCoordsAndType[i][1], spawnData.GhostCoordsAndType[i][2])));
             }
         }
 
@@ -118,6 +127,13 @@ namespace MoreLeviathanSpawns
         }*/
     }
 
+    public class GhostCoord
+    {
+        public UnityEngine.Vector3 Coords { get; set; }
+        //1 for "Adult", 2 for "Juvenile"
+        public int GhostType { get; set; }
+    }
+
     #region SpawnData
     //[Serializable]
     //Given these are static, could make it something other than an entire class?
@@ -127,49 +143,50 @@ namespace MoreLeviathanSpawns
         public float ReaperSpawnIntensity = 3;
         public float GhostSpawnIntensity = 3;
         //23 Reaper Coordinates
-        public float[][] ReaperCoords =
+        public UnityEngine.Vector3[] ReaperCoords =
         {
-            new float[]{ 120, -40, -568 }, //Grassy Plateaus, South
-            new float[]{ 1407, -190, 584 }, //Bulb Zone, East-North-East
-            new float[]{ 278, -175, 1398 }, //Mountains, North
-            new float[]{ -811, -240, -1240 }, //Grand Reef, South-South-West
-            new float[]{ -442, -132, -912 }, //Grand Reef, South
-            new float[]{ -310, -45, 92 }, //Kelp Forest, West
-            new float[]{ 190, -52, 477 }, //Kelp Forest, North
-            new float[]{ 500, -98, 318 }, //Mushroom Forest, East
-            new float[]{ 680, -85, 331 }, //Mushroom Forest, East
-            new float[]{ -172, -70, -781 }, //Crag Field, South
-            new float[]{ -692, -130, -725 }, //Sparse Reef, South-West
-            new float[]{ -745, -80, -1050 }, //Grand Reef, South-South-West (Under Floating Island)
-            new float[]{ -278, -30, -621 }, //Kelp Forest, South
-            new float[]{ -295, -45, -350 }, //Grassy Plateaus, South-West
-            new float[]{ -516, -110, 531 }, //Mushroom Forest, North-West
-            new float[]{ -815, -68, 316 }, //Mushroom Forest, West-North-West
-            new float[]{ -531, -60, -175 }, //Grassy Plateaus, West
-            new float[]{ -250, -142, 906 }, //Underwater Islands, North
-            new float[]{ -1122, -113, 710 }, //Mushroom Forest, North-West
-            new float[]{ -1190, -102, -527 }, //Sea Treader's Path, West-South-West
-            new float[]{ -754, -102, 1334 }, //Blood Kelp Zone, North-North-West
-            new float[]{ 432, -65, 690 }, //Kelp Forest, North-North-East
-            new float[]{ 383, -60, 40 } //Grassy Plateaus, East
+            new UnityEngine.Vector3( 120, -40, -568 ), //Grassy Plateaus, South
+            new UnityEngine.Vector3( 1407, -190, 584 ), //Bulb Zone, East-North-East
+            new UnityEngine.Vector3( 278, -175, 1398 ), //Mountains, North
+            new UnityEngine.Vector3( -811, -240, -1240 ), //Grand Reef, South-South-West
+            new UnityEngine.Vector3( -442, -132, -912 ), //Grand Reef, South
+            new UnityEngine.Vector3( -310, -45, 92 ), //Kelp Forest, West
+            new UnityEngine.Vector3( 190, -52, 477 ), //Kelp Forest, North
+            new UnityEngine.Vector3( 500, -98, 318 ), //Mushroom Forest, East
+            new UnityEngine.Vector3( 680, -85, 331 ), //Mushroom Forest, East
+            new UnityEngine.Vector3( -172, -70, -781 ), //Crag Field, South
+            new UnityEngine.Vector3( -692, -130, -725 ), //Sparse Reef, South-West
+            new UnityEngine.Vector3( -745, -80, -1050 ), //Grand Reef, South-South-West (Under Floating Island)
+            new UnityEngine.Vector3( -278, -30, -621 ), //Kelp Forest, South
+            new UnityEngine.Vector3( -295, -45, -350 ), //Grassy Plateaus, South-West
+            new UnityEngine.Vector3( -516, -110, 531 ), //Mushroom Forest, North-West
+            new UnityEngine.Vector3( -815, -68, 316 ), //Mushroom Forest, West-North-West
+            new UnityEngine.Vector3( -531, -60, -175 ), //Grassy Plateaus, West
+            new UnityEngine.Vector3( -250, -142, 906 ), //Underwater Islands, North
+            new UnityEngine.Vector3( -1122, -113, 710 ), //Mushroom Forest, North-West
+            new UnityEngine.Vector3( -1190, -102, -527 ), //Sea Treader's Path, West-South-West
+            new UnityEngine.Vector3( -754, -102, 1334 ), //Blood Kelp Zone, North-North-West
+            new UnityEngine.Vector3( 432, -65, 690 ), //Kelp Forest, North-North-East
+            new UnityEngine.Vector3( 383, -60, 40 ) //Grassy Plateaus, East
         };
         //14 Ghost Coordinates
-        public float[][] GhostCoordsAndType =
+        public GhostCoord[] GhostCoordsAndType =
         {
-            new float[]{ -284, -293, 1100, 1 }, //Adult, Underwater Islands, North
-            new float[]{ 1065, -211, 466, 1 }, //Adult, Bulb Zone, East-North-East
-            new float[]{ 876, -122, 881, 1 }, //Adult, Bulb Zone, North-East
-            new float[]{ -28, -318, 1296, 1 }, //Adult, Mountains, North
-            new float[]{ 10, -219, -220, 2 }, //Juvenile, Jellyshroom Cave, South
-            new float[]{ -396, -350, -925, 2 }, //Juvenile, Grand Reef, South
-            new float[]{ -958, -300, -540, 2 }, //Juvenile, Blood Kelp Trench, South-West
-            new float[]{ -988, -885, 400, 2 }, //Juvenile, Lost River, West-North-West (Tree Cove)
-            new float[]{ -695, -478, -993, 2 }, //Juvenile, Grand Reef, South-South-West (Degasi Base)
-            new float[]{ -618, -213, -82, 2 }, //Juvenile, Jellyshroom Cave, West
-            new float[]{ -34, -400, 926, 2 }, //Juvenile, Underwater Islands, North
-            new float[]{ -196, -436, 1056, 2 }, //Juvenile, Underwater Islands, North
-            new float[]{ 1443, -260, 883, 2 }, //Juvenile, Bulb Zone, North-East
-            new float[]{ 1075, -475, 944, 2 } //Juvenile, Mountains, North-East (Lost River Entrance)
+            //NOTE!! Should I be using "new int 1"?
+            new GhostCoord { Coords = new UnityEngine.Vector3( -284, -293, 1100 ), GhostType = 1 }, //Adult, Underwater Islands, North
+            new GhostCoord { Coords = new UnityEngine.Vector3( 1065, -211, 466 ), GhostType = 1 }, //Adult, Bulb Zone, East-North-East
+            new GhostCoord { Coords = new UnityEngine.Vector3( 876, -122, 881 ), GhostType = 1 }, //Adult, Bulb Zone, North-East
+            new GhostCoord { Coords = new UnityEngine.Vector3( -28, -318, 1296 ), GhostType = 1 }, //Adult, Mountains, North
+            new GhostCoord { Coords = new UnityEngine.Vector3( 10, -219, -220 ), GhostType = 2 }, //Juvenile, Jellyshroom Cave, South
+            new GhostCoord { Coords = new UnityEngine.Vector3( -396, -350, -925 ), GhostType = 2 }, //Juvenile, Grand Reef, South
+            new GhostCoord { Coords = new UnityEngine.Vector3( -958, -300, -540 ), GhostType = 2 }, //Juvenile, Blood Kelp Trench, South-West
+            new GhostCoord { Coords = new UnityEngine.Vector3( -988, -885, 400 ), GhostType = 2 }, //Juvenile, Lost River, West-North-West (Tree Cove)
+            new GhostCoord { Coords = new UnityEngine.Vector3( -695, -478, -993 ), GhostType = 2 }, //Juvenile, Grand Reef, South-South-West (Degasi Base)
+            new GhostCoord { Coords = new UnityEngine.Vector3( -618, -213, -82 ), GhostType = 2 }, //Juvenile, Jellyshroom Cave, West
+            new GhostCoord { Coords = new UnityEngine.Vector3( -34, -400, 926 ), GhostType = 2 }, //Juvenile, Underwater Islands, North
+            new GhostCoord { Coords = new UnityEngine.Vector3( -196, -436, 1056 ), GhostType = 2 }, //Juvenile, Underwater Islands, North
+            new GhostCoord { Coords = new UnityEngine.Vector3( 1443, -260, 883 ), GhostType = 2 }, //Juvenile, Bulb Zone, North-East
+            new GhostCoord { Coords = new UnityEngine.Vector3( 1075, -475, 944 ), GhostType = 2 } //Juvenile, Mountains, North-East (Lost River Entrance)
         };
     }
     #endregion
